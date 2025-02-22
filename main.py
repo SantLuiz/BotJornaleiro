a0487b9f4c1b3e8eab6c607ae98aaf430a1aaa27
SantLuiz <luizsaraujo1@gmail.com>
2025-02-21 23:55:01 -0300

    testando funcionalidades

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import telebot, os
+import telebot, os, emoji
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 
 load_dotenv(".env") 
@@ -6,9 +7,6 @@ ChaveApi = os.getenv("TOKEN_TELEBOT")
 
 if not ChaveApi:
     raise ValueError("A chave da API não foi encontrada no .env")
-
-print(f"Chave API carregada: {ChaveApi}")
-
 
 bot = telebot.TeleBot(ChaveApi)
 
@@ -32,24 +30,41 @@ def ConfigGeral(msg):
     bot.send_message(msg.chat.id, "Configuração de Assuntos")
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "noticias":
+        bot.answer_callback_query(call.id, "Aqui estão as suas noticias")
+    elif call.data == "concursos":
+        bot.answer_callback_query(call.id, "Aqui estão os concursos publicados recentemente")
+
 
 def verificar(msg):
     return True
 
 @bot.message_handler(func=verificar)
 def BoasVindas(msg):
+
+    markup = InlineKeyboardMarkup()
+
+    #btn1 = InlineKeyboardButton(emoji.emojize(":gear:"), url="https://www.google.com")
+    #btn2 = InlineKeyboardButton(emoji.emojize(":newspaper:"), callback_data="botao_clicado")
+     #markup.add(btn1, btn2)
+
+    markup.row(InlineKeyboardButton(emoji.emojize(":newspaper:"), callback_data="noticias"),
+               InlineKeyboardButton(emoji.emojize(":receipt:"), callback_data="concursos"),
+               InlineKeyboardButton(emoji.emojize(":gear:"), url="https://www.google.com"),)
+
+   
+
+
     textoMsg = f"""
 Olá,{msg.from_user.first_name}, 
 Seja Bem-Vindo ao Bot Jornaleiro
 
 Escolha uma das opções abaixo:
 (Clique no item)
-
-->  /ConfigurarGeral
-->  /ConfigurarFontes
-->  /ConfigurarAssuntos
 """
-    bot.send_message(msg.chat.id, textoMsg)
+    bot.send_message(msg.chat.id, textoMsg, reply_markup=markup)
 
 
 
